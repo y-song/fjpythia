ba0a52a04192157edd1f9fa17fc88e816d71a0e4
youqi <y.song@berkeley.edu>
2019-12-26 19:42:12 -0800

    Only saved variables for comparing with and without nPDF cross sections

--- src/youqi/fj_and_root.cxx
+++ src/youqi/fj_and_root.cxx
@@ -10,7 +10,6 @@
 #include <TTree.h>
 #include <TString.h>
 #include <TMath.h>
-#include <math.h>
 
 #include <fastjet/Selector.hh>
 #include <fastjet/PseudoJet.hh>
@@ -38,28 +37,21 @@ int fj_and_root()
 	// new branches
 	UInt_t ntrials, evid;
 	Float_t xsec, x, y, Q2, W2;
+	Float_t pt_jet, phi_jet;
 	Float_t phi_photon;
 
-	// new particle variables
-	std::vector<float> pt_jet;
-	std::vector<float> phi_jet;
-	std::vector<float> z;
-	std::vector<float> j;
-
 	// initialize TTree
-	TTree *tree1 = new TTree("Tree1", "Tree1");
-	tree1->Branch("ntrials", &ntrials, "ntrials/I");
-	tree1->Branch("evid", &evid, "evid/I");
-	tree1->Branch("xsec", &xsec, "xsec/F");
-	tree1->Branch("x", &x, "x/F");
-	tree1->Branch("y", &y, "y/F");
-	tree1->Branch("Q2", &Q2, "Q2/F");
-	tree1->Branch("W2", &W2, "W2/F");
-	tree1->Branch("phi_photon", &phi_photon, "phi_photon/F");
-	tree1->Branch("pt_jet", &pt_jet);
-	tree1->Branch("phi_jet", &phi_jet);
-	tree1->Branch("z", &z);
-	tree1->Branch("j", &j);
+	TTree *tree = new TTree("Tree", "Tree");
+	tree->Branch("ntrials", &ntrials, "ntrials/I");
+	tree->Branch("evid", &evid, "evid/I");
+	tree->Branch("xsec", &xsec, "xsec/F");
+	tree->Branch("x", &x, "x/F");
+	tree->Branch("y", &y, "y/F");
+	tree->Branch("Q2", &Q2, "Q2/F");
+	tree->Branch("W2", &W2, "W2/F");
+	tree->Branch("pt_jet", &pt_jet, "pt_jet/F");
+	tree->Branch("phi_jet", &phi_jet, "phi_jet/F");
+	tree->Branch("phi_photon", &phi_photon, "phi_photon/F");
 
 	// intialize PYTHIA
 	Pythia pythia;
@@ -72,7 +64,7 @@ int fj_and_root()
 	}
 
 	// generate and analyze events
-	int nEv = args.getOptInt("--nev", 4); // default will be 4 event(!)
+	int nEv = args.getOptInt("--nev", 1); // default will be 1 event(!)
 	double jetR = args.getOptDouble("--jetR", 1.0);
 	double minJetPt = args.getOptDouble("--minJetPt", 0.0);
 	double maxPartEta = std::abs(args.getOptDouble("--maxParticleEta", 4.5));
@@ -89,26 +81,20 @@ int fj_and_root()
 	fj::Selector partSelector = fastjet::SelectorAbsEtaMax(maxPartEta) * fastjet::SelectorPtMin(minPartPt);
 	fj::Selector jetSelector = fastjet::SelectorAbsEtaMax(maxPartEta - jetR - 0.01) * fastjet::SelectorPtMin(minJetPt);
 
-	// Begin event loop. Generate event. Skip if error..
-	int num_jet = 0;
+	// Begin event loop. Generate event. Skip if error. List first few.
 	LoopUtil::TPbar pbar(nEv);
 	for (int iEvent = 0; iEvent < nEv; ++iEvent)
 	{
 		pbar.Update();
 		if (!pythia.next())
 			continue;
-		
-	        z.clear();
-       		j.clear();
-		pt_jet.clear();
-		phi_jet.clear();
 
-		// four-momenta of proton, electron, virtual photon/Z^0/W^+-.
+		// Four-momenta of proton, electron, virtual photon/Z^0/W^+-.
 		Vec4 pProton = event[1].p();
 		Vec4 peIn = event[4].p();
 		Vec4 peOut = event[6].p();
 		Vec4 pPhoton = peIn - peOut;
-	
+		
 		// Q2, W2, Bjorken x, y, nu.
 		Q2 = -pPhoton.m2Calc();
 		W2 = (pProton + pPhoton).m2Calc();
@@ -118,45 +104,27 @@ int fj_and_root()
 		auto parts = FJUtils::getPseudoJetsFromPythia(&pythia, true); // only_final==true
 		std::vector<fj::PseudoJet> parts_selected = partSelector(parts);
 
-		evid = iEvent;
-		xsec = pythia.info.sigmaGen();
-		ntrials = pythia.info.nTried();
-		phi_photon = pPhoton.phi();
-
 		// run jet finding
 		fj::JetDefinition jet_def(fj::antikt_algorithm, jetR);
 		fj::ClusterSequence ca(parts_selected, jet_def);
 		std::vector<fj::PseudoJet> jets_inclusive = ca.inclusive_jets();
 		std::vector<fj::PseudoJet> jets = jetSelector(jets_inclusive);
 
-		// loop over jets
+		// write jet properties to a TTree
 		for (unsigned int ij = 0; ij < jets.size(); ij++)
 		{
-			// loop over particles
-			for (int i = 0; i < jets[ij].constituents().size(); i++)
-			{
-				Pythia8::Particle *_p = jets[ij].constituents()[i].user_info<FJUtils::PythiaUserInfo>().getParticle();
-               			// save only charged hadrons
-                		if (_p->isCharged() && _p->isHadron())
-                		{
-               	     			double pxj, pyj, pzj, pxh, pyh, pzh, p_jet, cross;
-					pxj = jets[ij].px();
-                    			pyj = jets[ij].py();
-                    			pzj = jets[ij].pz();
-                    			pxh = _p->px();
-                    			pyh = _p->py();
-                    			pzh = _p->pz();
-                    			p_jet = sqrt(pxj*pxj + pyj*pyj + pzj*pzj);
-                   			cross = sqrt( pow((pyj*pzh-pyh*pzj),2.0) + pow((pxj*pzh-pzj*pxh),2.0) + pow((pxj*pyh-pyj*pxh),2.0) );
-                    			z.push_back((pxj*pxh + pyj*pyh + pzj*pzh) / (p_jet*p_jet));
-                    			j.push_back(cross / p_jet);
-				    	pt_jet.push_back(jets[ij].perp());
-        			    	phi_jet.push_back(jets[ij].phi());
-                		}
-			}	
-		}	
-		tree1->Fill();
 
+			evid = iEvent;
+			xsec = pythia.info.sigmaGen();
+			ntrials = pythia.info.nTried();
+			pt_jet = jets[ij].perp();
+			phi_jet = jets[ij].phi();
+			phi_photon = pPhoton.phi();
+			
+			tree->Fill();
+	
+		} //jet
+		
 	} //event
 
 	// write and close the output file
